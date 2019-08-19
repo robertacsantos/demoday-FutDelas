165a29f5bd59d582f0b113c8c11d1b0e8b0f0ae6
Nikolly Amancio <nikollyamancio2@gmail.com>
2019-08-19 17:50:21 -0300

    atualizando

--- funcionalidades/static/js/script.js
+++ funcionalidades/static/js/script.js
@@ -3,3 +3,15 @@ function participar()
 {
     alert("Parabéns, você foi convocada para esse jogo!");
 }
+
+
+var clicks = 1;
+function ocultaForm() {
+  alert('VOCÊ CLICOU NA IMAGEM!');
+  if (clicks === 1) {
+    console.log('Você clicou ' + clicks + ' vez na imagem!');
+  } else {
+    console.log('Você clicou ' + clicks + ' vezes na imagem!');
+  }
+  clicks++;
+}
