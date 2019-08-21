6ec50085d075f3e30f8345d6d88c5e96b1c1cf5b
Roberta <rcsantos2019@gmail.com>
2019-08-21 20:42:40 -0300

    Config Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn futdelas.wsgi
--- futdelas/settings.py
+++ futdelas/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+django_heroku.settings(locals())
